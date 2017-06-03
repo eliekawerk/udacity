803c56e4084c2babd80470607a40a43e676a925b
Srikanth Pagadala <srikanth.pagadala@gmail.com>
2017-06-03 12:26:47 -0700

    fixed install-mac.sh by adding line for OPENSSL_ROOT_DIR

--- Self-Driving Car Engineer Nanodegree/ParticleFilter-P3/install-mac.sh
+++ Self-Driving Car Engineer Nanodegree/ParticleFilter-P3/install-mac.sh
@@ -4,6 +4,7 @@ cd uWebSockets
 git checkout e94b6e1
 patch CMakeLists.txt < ../cmakepatch.txt
 mkdir build
+export OPENSSL_ROOT_DIR=/usr/local/Cellar/openssl/1.0.2l
 export PKG_CONFIG_PATH=/usr/local/opt/openssl/lib/pkgconfig 
 cd build
 cmake ..
